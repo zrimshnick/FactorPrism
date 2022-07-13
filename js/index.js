341a10e8d232c05963a260b053c583d128fef315
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2022-07-13 13:49:53 -0400

    fades

--- js/index.js
+++ js/index.js
@@ -1,9 +1,25 @@
 console.log("Connected");
 
-$(document).on("scroll", function () {
+/* $(document).on("scroll", function () {
   var pageTop = $(document).scrollTop();
   var pageBottom = pageTop + $(window).height();
   var tags = $(".tag");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+}); */
+
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".illustrated");
 
   for (var i = 0; i < tags.length; i++) {
     var tag = tags[i];
