fb969a93bea5a3687df50b96a10e994497abd93a
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2022-07-13 18:23:39 -0400

    more animations

--- js/index.js
+++ js/index.js
@@ -15,7 +15,7 @@ console.log("Connected");
     }
   }
 }); */
-
+/////////ILLUSTRATED
 $(document).on("scroll", function () {
   var pageTop = $(document).scrollTop();
   var pageBottom = pageTop + $(window).height();
@@ -28,6 +28,22 @@ $(document).on("scroll", function () {
       $(tag).addClass("visible");
     } else {
       $(tag).removeClass("visible");
+    }
+  }
+});
+/////VIDEO
+$(document).on("scroll", function () {
+  var pageTop2 = $(document).scrollTop();
+  var pageBottom2 = pageTop2 + $(window).height();
+  var tags2 = $(".video-section");
+
+  for (var i = 0; i < tags2.length; i++) {
+    var tag2 = tags2[i];
+
+    if ($(tag2).position().top < pageBottom2) {
+      $(tag2).addClass("visible");
+    } else {
+      $(tag2).removeClass("visible");
     }
   }
 });
