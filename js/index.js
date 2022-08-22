569e33e98e5af83eb1f000d9faa6adc9ed62c60c
ZackRimshnick <zachary.rimshnick21@sitechhs.com>
2022-08-22 17:56:59 -0400

    whole lotta stuff

--- js/index.js
+++ js/index.js
@@ -80,6 +80,38 @@ $(document).on("scroll", function () {
     }
   }
 });
+////////FEATURES
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".feat-enter");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
+$(document).on("scroll", function () {
+  var pageTop = $(document).scrollTop();
+  var pageBottom = pageTop + $(window).height();
+  var tags = $(".feat-highlights");
+
+  for (var i = 0; i < tags.length; i++) {
+    var tag = tags[i];
+
+    if ($(tag).position().top < pageBottom) {
+      $(tag).addClass("visible");
+    } else {
+      $(tag).removeClass("visible");
+    }
+  }
+});
+
 ///////////////// FACTS - WORDS TYPING
 var words = [
     "Completely Automatic",
